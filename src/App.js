d157cc1516f731d726866baf5f56702be040af8b
David <davidmejia.05@hotmail.com>
2023-06-01 13:42:27 -0600

    nc2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { ConstructionNotice } from './components/ConstructionNotice/ConstructionNotice';
-import { NewNavbar } from './components/common/header/Header';
+import { NewNavbar } from './components/common/navbar/Navbar';
 //importing bootstrap 5 css
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-
 function App() {
   return (
-    <div>
+    <div className="App">
       <NewNavbar></NewNavbar>    
     </div>
   );
