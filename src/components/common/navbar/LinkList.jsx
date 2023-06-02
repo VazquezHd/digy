c34b3a8d83f2fcc30efc8e20eaf8bd767e09c361
David <davidmejia.05@hotmail.com>
2023-06-02 12:57:28 -0600

    icons

--- src/components/common/navbar/LinkList.jsx
+++ src/components/common/navbar/LinkList.jsx
@@ -1,22 +1,30 @@
-export const Link = ({link}) => {
-    return (
+import * as Icons from "react-icons/bs";
 
+const CustomIcon = ({ name }) => {
+  let Icon = Icons[name];
+  if (!Icon || name === undefined) {
+    Icon = Icons["BsQuestionCircle"];
+  }
+  return <Icon />;
+};
 
-        <li className="nav-item">
-          <a href="" className="nav-link text-small pb-0">
-          <br/>
-            {link.name} <br/>{link.description}
-          </a>
-        </li>
-      )
+export const Link = ({ link }) => {
+  return (
+    <li className="nav-item">
+      <a href="" className="nav-link text-small pb-0">
+        <CustomIcon name={link.icon} /> {link.name} <br />
+        {link.description}
+      </a>
+    </li>
+  );
+};
 
-}
 export const LinkList = ({ links }) => {
   return (
     <div className="col-lg-4 mb-4">
       <ul className="list-unstyled">
         {links.map((link, index) => (
-          <Link link={link} key={index}/>
+          <Link link={link} key={index} />
         ))}
       </ul>
     </div>
