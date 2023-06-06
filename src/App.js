69bc0591250a7bd945ea6c96a88c2b97152521e8
David <davidmejia.05@hotmail.com>
2023-06-06 14:59:49 -0600

    routers

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import { Home } from './components/home/Home';
 import { Service } from './components/services/Services';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 let service_data = {
@@ -14,18 +15,21 @@ let service_data = {
   name: "Internet Dedicado Simétrico",
   description: "Ofrecemos enlaces dedicados, con el más alto SLA del mercado y la mejor experiencia de conectividad segura para las aplicaciones de misión crítica que requiere tu empresa.",
   title2: "¿Qué nos diferencia?",
-  texto: "Es la tecnología de telecomunicaciones que se basa en la Voz sobre IP (Voice Over Internet Protocol o Voz sobre Protocolo de Internet). Es la telefonía basada en la red que permite hacer llamadas desde cualquier lugar y dispositivo."
+  text: "Es la tecnología de telecomunicaciones que se basa en la Voz sobre IP (Voice Over Internet Protocol o Voz sobre Protocolo de Internet). Es la telefonía basada en la red que permite hacer llamadas desde cualquier lugar y dispositivo."
 }
 
 
 function App() {
 
   return (
-    <div className="App">
-      <Navbar/>    
-      <Home/>
-      <Service data={service_data}/>
-    </div>
+<BrowserRouter>
+<Navbar/>  
+<Routes>
+  <Route exact path="/" element={<Home />}/>
+  <Route exact path="/internet-empresarial" element={<Service data={service_data}/>}/>
+</Routes>
+</BrowserRouter>
+
   );
 }
 
