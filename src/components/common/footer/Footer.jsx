cbc4bdde255b2931bd7efac95ebed78c88c949db
David <dmg_92@hotmail.com>
2023-06-09 08:59:42 -0600

    footer

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -2,9 +2,9 @@ import { ContactUs } from "./contactus/ContactUs"
 import { Contact } from "./contact/Contact"
 
 export const Footer = () => {
-    return (<>
+    return (<footer>
         <ContactUs/>
         <Contact/>
 
-    </>)
+    </footer>)
 }
